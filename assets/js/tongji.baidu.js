e2c6999429234780ea2362601b2e5dec63b534aa
东篱先生 <admin@buerya.cn>
2023-04-13 09:38:38 +0800

    Site updated: 2023-04-13 09:38:36

---
+++ assets/js/tongji.baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?201056ecd8ffd09f6d28ab64cfa95a00";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
